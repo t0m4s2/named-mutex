7e909c6cb63a2385b1e9e4f5dfb0abce897fa7aa
Tomasz Wilk <tomasz.wilk@gmail.com>
2018-12-22 13:07:07 -0500

    Add support for process mutex

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SRC = src/main.cc src/named_mutex.cc src/named_mutex_impl.cc
 OBJ = $(SRC:.cpp=.o)
-LIBS := -pthread
+LIBS := -pthread -lrt
 
 named_mutex : $(OBJ)
 	$(CXX) -g -o $@ -I include $(OBJ) $(LIBS)
--- src/main.cc
+++ src/main.cc
@@ -15,17 +15,6 @@ int main(void) {
 
 )banner";
 
-  const char* PROCESS_LOCK_BANNER = R"banner(
-
-                                     _            _
- _ __  _ __ ___   ___ ___  ___ ___  | | ___   ___| | __
-| '_ \| '__/ _ \ / __/ _ \/ __/ __| | |/ _ \ / __| |/ /
-| |_) | | | (_) | (_|  __/\__ \__ \ | | (_) | (__|   <
-| .__/|_|  \___/ \___\___||___/___/ |_|\___/ \___|_|\_\
-|_|
-
-)banner";
-
   std::cout << THREAD_LOCK_BANNER;
 
   auto tm = tw::CreateNamedMutex("test", false);
@@ -55,5 +44,44 @@ int main(void) {
   std::cout << "MAIN: waiting for child thread to finish" << std::endl;
   threadObj.join();
 
+  const char* PROCESS_LOCK_BANNER = R"banner(
+
+                                     _            _
+ _ __  _ __ ___   ___ ___  ___ ___  | | ___   ___| | __
+| '_ \| '__/ _ \ / __/ _ \/ __/ __| | |/ _ \ / __| |/ /
+| |_) | | | (_) | (_|  __/\__ \__ \ | | (_) | (__|   <
+| .__/|_|  \___/ \___\___||___/___/ |_|\___/ \___|_|\_\
+|_|
+
+)banner";
+
   std::cout << PROCESS_LOCK_BANNER;
+
+  auto pm = tw::CreateNamedMutex("test", false);
+  if (pm->Init())
+  {
+    std::cout << "Mutex initialization failed" << std::endl;
+    return -1;
+  }
+  std::cout << "MAIN: created mutex \"test\'" << std::endl;
+
+  std::cout << "MAIN: test->lock" << std::endl;
+  pm->Lock();
+  std::cout << "MAIN: got lock" << std::endl;
+
+  std::thread threadObj2([=]{
+                          std::cout << "  CHILD: test->lock" << std::endl;
+                          pm->Lock();
+                          std::cout << "  CHILD: got lock" << std::endl;
+                          pm->UnLock();
+                          std::cout << "  CHILD: test->unlock" << std::endl;
+                        });
+
+  std::cout << "MAIN: sleep 5 sec" << std::endl;
+  usleep(5000000);
+  std::cout << "MAIN: test->unlock" << std::endl;
+  pm->UnLock();
+  std::cout << "MAIN: waiting for child thread to finish" << std::endl;
+  threadObj2.join();
+
 }
